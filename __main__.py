efffa77e33d22d1e751505977c1290c717162add
Davis Yoshida <davis.yoshida@gmail.com>
2016-03-27 06:23:29 -0600

    Added requirements.txt and moved channel to config.py

--- __main__.py
+++ __main__.py
@@ -4,6 +4,6 @@ from emotebot import EmoteBot
 
 
 if __name__ == "__main__":
-    bot = EmoteBot(config.TOKEN, 'bot-test')
+    bot = EmoteBot(config.TOKEN, config.CHANNEL)
     loop = asyncio.get_event_loop()
     loop.run_until_complete(bot.main_loop())
