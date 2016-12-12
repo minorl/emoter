a5ebfc6e38e1a1bc826d15dea2ce6dd9be4c7893
Spiffyman <malnackjm@gmail.com>
2016-12-12 12:42:31 -0800

    Added hankey command

--- jeff_bot.py
+++ jeff_bot.py
@@ -10,13 +10,22 @@ class JeffBot(SlackBot):
         self.pig_name = "Pig Latin"
         self.pig_expr = CaselessLiteral("pig") + symbols.tail.setResultsName("message") + StringEnd()
         self.pig_doc = "Translate a message to pig latin"
+        self.hankey_name = "Hankey"
+        self.hankey_expr = CaselessLiteral("hankey") + symbols.user_name.setResultsName("target") + StringEnd()
+        self.hankey_doc = "Target user for hankeying"
+        self.target = jeff_bot_target
 
     @register(channels='channels')
     async def command_jeff(self, user, in_channel, message):
-        if user == "skelni":
-            return ReactCommand("shit")
+        if user == self.target:
+            return ReactCommand("hankey")
         else:
             return None
+
+    @register(name="hankey_name", expr="hankey_expr", doc="hankey_doc")
+    async def command_hankey(self, user, in_channel, parsed):
+        self.target = parsed["target"]
+        return None
 
     @register(name="pig_name", expr="pig_expr", doc="pig_doc")
     async def command_pig_latin(self, user, in_channel, parsed):
