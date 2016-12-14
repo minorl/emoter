cd308a446d277f3853836878ac414546bcfedc46
Spiffyman <malnackjm@gmail.com>
2016-12-13 17:17:52 -0800

    Replaced cut down main

--- __main__.py
+++ __main__.py
@@ -1,20 +1,20 @@
 """Main module for Slack bot"""
 import argparse
 import asyncio
-# import binder_bot
+import binder_bot
 import config
-# import emote_bot
-# import haiku_bot
+import emote_bot
+import haiku_bot
 import jeff_bot
 from mongoengine import connect
-# import quote_bot
-# import react_bot
-# import sentiment_bot
-# import twitch_bot
-# import wordcloud_bot
+import quote_bot
+import react_bot
+import sentiment_bot
+import twitch_bot
+import wordcloud_bot
 
 from slack.slack_api import Slack, SlackConfig
-# import tensorflow as tf
+import tensorflow as tf
 
 
 def main():
@@ -32,28 +32,28 @@ def main():
         load_history=args.load_history)
 
     slackapp = Slack(slack_config)
-    # emote_bot.EmoteBot(channels=config.EMOJI_CHANNELS, slack=slackapp)
-    # binder_bot.BinderBot(admins=config.ADMINS, max_len=config.MAX_BIND_LEN, slack=slackapp)
-    # react_bot.ReactBot(
-    #     admins=config.ADMINS,
-    #     out_channels=config.REACTION_CHANNELS,
-    #     max_per_user=config.MAX_REACTS_PER_CHANNEL,
-    #     slack=slackapp)
-    # quote_bot.QuoteBot(slack=slackapp)
-    # wordcloud_bot.WordcloudBot(slack=slackapp)
+    emote_bot.EmoteBot(channels=config.EMOJI_CHANNELS, slack=slackapp)
+    binder_bot.BinderBot(admins=config.ADMINS, max_len=config.MAX_BIND_LEN, slack=slackapp)
+    react_bot.ReactBot(
+        admins=config.ADMINS,
+        out_channels=config.REACTION_CHANNELS,
+        max_per_user=config.MAX_REACTS_PER_CHANNEL,
+        slack=slackapp)
+    quote_bot.QuoteBot(slack=slackapp)
+    wordcloud_bot.WordcloudBot(slack=slackapp)
     jeff_bot.JeffBot(emoji_random=config.EMOJI_RANDOM, jeff_bot_target=config.JEFF_BOT_TARGET, jeff_channels=config.JEFF_CHANNELS, slack=slackapp)
 
-    # twitch_alias = 'twitch_db'
-    # connect(config.TWITCH_DB_NAME, alias=twitch_alias)
+    twitch_alias = 'twitch_db'
+    connect(config.TWITCH_DB_NAME, alias=twitch_alias)
 
-    # twitch_bot.TwitchBot(twitch_alias, min_length=config.MIN_MARKOV_LENGTH, slack=slackapp)
+    twitch_bot.TwitchBot(twitch_alias, min_length=config.MIN_MARKOV_LENGTH, slack=slackapp)
 
-    # haiku_bot.HaikuBot(slack=slackapp)
+    haiku_bot.HaikuBot(slack=slackapp)
 
-    # with tf.Graph().as_default(), tf.Session() as session:
-        # sentiment_bot.SentimentBot(session=session, slack=slackapp)
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(slackapp.run())
+    with tf.Graph().as_default(), tf.Session() as session:
+        sentiment_bot.SentimentBot(session=session, slack=slackapp)
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(slackapp.run())
 
 
 if __name__ == '__main__':
