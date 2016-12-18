83ef4a5a0abfc3ab07806ad281cc3e10008f4813
Spiffyman <malnackjm@gmail.com>
2016-12-17 20:44:45 -0800

    Changed from randint to random and replaced double quotes

--- jeff_bot.py
+++ jeff_bot.py
@@ -1,52 +1,53 @@
 from pyparsing import CaselessLiteral, StringEnd
-from random import randint, choice
+from random import random, choice
 from slack.bot import SlackBot, register
 from slack.command import MessageCommand, ReactCommand
 from slack.parsing import symbols
 
 
 class JeffBot(SlackBot):
-    def __init__(self, jeff_bot_emojis, jeff_channels, jeff_bot_target, slack=None):
+    def __init__(self, jeff_bot_probability, jeff_bot_emojis, jeff_channels, jeff_bot_target, slack=None):
         self.channels = jeff_channels
-        self.pig_name = "Pig Latin"
-        self.pig_expr = CaselessLiteral("pig") + symbols.tail.setResultsName("message") + StringEnd()
-        self.pig_doc = "Translate a message to pig latin"
-        self.hankey_name = "Hankey"
-        self.hankey_expr = CaselessLiteral("hankey") + symbols.user_name.setResultsName("target") + StringEnd()
-        self.hankey_doc = "Target user for hankeying"
+        self.pig_name = 'Pig Latin'
+        self.pig_expr = CaselessLiteral('pig') + symbols.tail.setResultsName('message') + StringEnd()
+        self.pig_doc = 'Translate a message to pig latin'
+        self.hankey_name = 'Hankey'
+        self.hankey_expr = CaselessLiteral('hankey') + symbols.user_name.setResultsName('target') + StringEnd()
+        self.hankey_doc = 'Target user for hankeying'
         self.target = jeff_bot_target
         self.ramoji = jeff_bot_emojis
+        self.probability = jeff_bot_probability
 
-    @register(channels="channels")
+    @register(channels='channels')
     async def command_random(self, user, in_channel, message):
-        if user == "skelni" and randint(0,19) == 1:
+        if random() < self.probability:
             return ReactCommand(choice(self.ramoji))
         else:
             return None
 
-    @register(channels="channels")
+    @register(channels='channels')
     async def command_jeff(self, user, in_channel, message):
         if user == self.target:
-            return ReactCommand("hankey")
+            return ReactCommand('hankey')
         else:
             return None
 
-    @register(name="hankey_name", expr="hankey_expr", doc="hankey_doc")
+    @register(name='hankey_name', expr='hankey_expr', doc='hankey_doc')
     async def command_hankey(self, user, in_channel, parsed):
-        self.target = parsed["target"] if user != "spiffyman" and user != "davis.yoshida" else user
+        self.target = parsed['target']
         return None
 
-    @register(name="pig_name", expr="pig_expr", doc="pig_doc")
+    @register(name='pig_name', expr='pig_expr', doc='pig_doc')
     async def command_pig_latin(self, user, in_channel, parsed):
-        message = parsed["message"]
+        message = parsed['message']
         return MessageCommand(text=pig_latin(message), channel=in_channel, user=user)
 
 def pig_latin(words):
-    vowels = {"a", "e", "i", "o", "u"}
+    vowels = {'a', 'e', 'i', 'o', 'u'}
     new = []
     for word in words.lower().split():
         if word[0] in vowels:
-            new.append(word + "way")
+            new.append(word + 'way')
         else:
             start = 0
             for letter in list(word):
@@ -54,5 +55,5 @@ def pig_latin(words):
                     break
                 else:
                     start += 1
-            new.append(word[start:] + word[:start] + "ay")
-    return " ".join(new)
+            new.append(word[start:] + word[:start] + 'ay')
+    return ' '.join(new)
