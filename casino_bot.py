a2c060b9e88406b3c594632fc3b767f267ad7acf
Leslie Minor <minorl@colorado.edu>
2017-01-13 23:22:33 -0700

    Address code review comments.

--- casino_bot.py
+++ casino_bot.py
@@ -11,11 +11,11 @@ class CasinoBot(SlackBot):
         self.currency_name = currency_name
 
         self.slots_name = 'Slot machine'
-        self.slots_expr = CaselessLiteral('slots')+symbols.int_num.setResultsName('bet')+StringEnd()
-        self.slots_doc = ('Gamble away your hard earned {0}:\n \tslot <currency bet>'.format(currency_name))
+        self.slots_expr = CaselessLiteral('slots') + symbols.int_num.setResultsName('bet') + StringEnd()
+        self.slots_doc = ('Gamble away your hard earned {0}:\n\tslot <currency bet>'.format(currency_name))
         self.slots_jackpot_symbol = ':moneybag:'
-        self.slots_sym = [':peach:', ':tangerine:', ':cherries:', ':watermelon:', ':banana:',self.slots_jackpot_symbol]
-        self.slots_contribution = .5
+        self.slots_sym = [':peach:', ':tangerine:', ':cherries:', ':watermelon:', ':banana:', self.slots_jackpot_symbol]
+        self.slots_contribution = .45
         #quick hack to persist jackpot across restarts
         self.slots_jackpot_file = Path('jackpot.txt')
         if self.slots_jackpot_file.is_file():
@@ -32,16 +32,20 @@ class CasinoBot(SlackBot):
         msg = ''
         if bank >= bet:
             reels = list(random.choice(self.slots_sym, 3))
-            won = bet * calculate_payout_multiple(reels, self.slots_jackpot_symbol)
+            jacks = reels.count(jackpot)
+            if jacks:
+                won = self.slots_jackpot if jacks == 3 else bet * 4 if jacks == 2 else 0
+            else:
+                won = bet * 10 if reels.count(reels[0]) == 3 else 0
+            
             if won:
-                if won == 30:
-                    won = self.slots_jackpot
+                if won == self.slots_jackpot:
                     self.update_jackpot(-won)
                     msg = "JACKPOT!!!! "
                 msg += 'You won {0} {1}!'.format(won, self.currency_name)
             else:
                 msg = 'You lose.'
-                self.update_jackpot(bet*self.slots_contribution)
+                self.update_jackpot(bet * self.slots_contribution)
             await economy.give(user, won - bet)
             return MessageCommand(text='{0}\n{1} Jackpot is {2}.'.format("".join(reels), msg, int(self.slots_jackpot)), channel=in_channel, user=user)
         else:
@@ -51,10 +55,5 @@ class CasinoBot(SlackBot):
         self.slots_jackpot += amount
         self.slots_jackpot_file.write_text(str(self.slots_jackpot))
 
-def calculate_payout_multiple(reels, jackpot):
-    jacks = reels.count(jackpot)
-    if jacks:
-        return 30 if jacks > 2 else 4 if jacks == 2 else 1
-    else:
-        return 10 if reels.count(reels[0]) == 3 else 0
+
 
